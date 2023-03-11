17e34bd34f6d5e43dd90fb80d2105e496fa86c9c
Benson Ndiwa <bnkimtai@gmail.com>
2023-03-11 10:34:03 +0300

    Icons

--- components/todoItem.js
+++ components/todoItem.js
@@ -1,16 +1,24 @@
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity} from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons'; 
+
 
 export default function TodoItem({ item, pressHandler }){
     return(
         <TouchableOpacity onPress={()=> pressHandler(item.key)}>
-            <Text style={styles.item}>{item.text}</Text>
+            <View style={styles.item}>
+                <Text>{item.text}</Text>
+                <MaterialIcons name="delete" size={24} color="#333" />
+            </View>
+            
         </TouchableOpacity>
     )
 }
 
 const styles = StyleSheet.create({
     item: {
+        flexDirection:'row',
+        justifyContent:'space-between',
         padding: 16,
         marginTop: 16,
         borderColor: '#bbb',
